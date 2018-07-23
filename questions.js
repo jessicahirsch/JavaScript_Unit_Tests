de1883757a421f0bcab9228e3bc9b6f201204b46
Jessica Hirsch <jessicahirsch@Jessicas-MBP.lan1>
2018-07-23 12:43:03 -0400

    Remove unecessary text

---
+++ questions.js
@@ -0,0 +1,4 @@
+
+
+document.evaluate(
+"//html/body/app/main-component/div/index-component/div/requests-component/div/request-view/div/div[2]/div/div/request-header/div/div/div[1]/request-header-trip/div/div/div", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
